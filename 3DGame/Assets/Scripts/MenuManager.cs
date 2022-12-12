f949d365aaa98a86bbf45abccf3dcbbea03db752
ploki0258 <57485339+ploki0258@users.noreply.github.com>
2022-12-12 17:57:06 +0800

    選單介面

--- 3DGame/Assets/Scripts/MenuManager.cs
+++ 3DGame/Assets/Scripts/MenuManager.cs
@@ -1,6 +1,31 @@
-﻿using UnityEngine;
+﻿using System.Net.Http.Headers;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
-public class MenuManager : MonoBehaviour
+public class MenuManager : MonoBehaviour,IBeginDragHandler,IEndDragHandler
 {
-    
+    public GameObject objBtn;
+
+    private RectTransform contents;
+    private ScrollRect scrollRect;
+
+    private float taragePoint;
+    private int count = 12;
+    private float delta_x;
+    private bool isRoll;
+
+    private void Start()
+    {
+        contents = transform.Find("Viewport/Content") as RectTransform;
+        scrollRect = transform.GetComponent<ScrollRect>();
+
+        delta_x = Mathf.CeilToInt(count / 3) * 720f;
+        contents.sizeDelta = new Vector2(delta_x, contents.sizeDelta.y);
+    }
+
+    private void leftBtn()
+    {
+        
+    }
 }
