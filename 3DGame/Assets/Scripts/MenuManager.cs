b0acf85c9613a3e8cd7fe25124fc484464101aa7
ploki0258 <57485339+ploki0258@users.noreply.github.com>
2022-12-16 16:17:56 +0800

    新增選單滑動功能

--- 3DGame/Assets/Scripts/MenuManager.cs
+++ 3DGame/Assets/Scripts/MenuManager.cs
@@ -3,29 +3,62 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class MenuManager : MonoBehaviour,IBeginDragHandler,IEndDragHandler
+public class MenuManager : MonoBehaviour, IBeginDragHandler, IEndDragHandler
 {
-    public GameObject objBtn;
+    public GameObject obj = null;
+    
+    // 獲取組件
+    private ScrollRect Rect;
+    // 紀錄圖片相應的位置
+    private float[] posArray = new float[] { 360f, 1080f, 1800f };
+    // 設置滾動的目標位置
+    private float targetPos;
+    // 判斷是否正在拖拽
+    private bool isDrag = false;
+    private int index = 0;
 
-    private RectTransform contents;
-    private ScrollRect scrollRect;
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        isDrag = true;
+    }
 
-    private float taragePoint;
-    private int count = 12;
-    private float delta_x;
-    private bool isRoll;
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        isDrag = false;
+
+        // 獲取當前的位置
+        Vector2 pos = Rect.normalizedPosition;
+        // 保留與第一張圖片的位置
+        float x = Mathf.Abs(pos.x - posArray[0]);
+
+        for (int i = 0; i < 3; i++)
+        {
+            float temp = Mathf.Abs(pos.x - posArray[i]);
+            if (temp < x)
+            {
+                x = temp;
+                index = i;
+            }
+        }
+
+        targetPos = posArray[index];
+    }
 
     private void Start()
     {
-        contents = transform.Find("Viewport/Content") as RectTransform;
-        scrollRect = transform.GetComponent<ScrollRect>();
+        Rect = GetComponent<ScrollRect>();
+        // contents = transform.Find("Viewport/Content") as RectTransform;
 
-        delta_x = Mathf.CeilToInt(count / 3) * 720f;
-        contents.sizeDelta = new Vector2(delta_x, contents.sizeDelta.y);
+        // delta_x = Mathf.CeilToInt(count / 3) * 720f;
+        // contents.sizeDelta = new Vector2(delta_x, contents.sizeDelta.y);
     }
 
-    private void leftBtn()
+    private void Update()
     {
-        
+        if (isDrag == true)
+        {
+            Rect.horizontalNormalizedPosition = Mathf.Lerp(Rect.horizontalNormalizedPosition,targetPos,Time.deltaTime * 4);
+        }
     }
+
 }
